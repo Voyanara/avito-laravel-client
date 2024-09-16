7f246ada16cad59a755afaa96b369303012a5ab5
Voyanara <darkallenb1394@gmail.com>
2024-09-16 04:23:18 +0300

    Add GetUserOperationHistory functionality to UserInfo class
    Include necessary exceptions and response classes for handling user operation history. Implement method to retrieve operations history based on date range.

--- src/Application/Services/Facades/UserInfo.php
+++ src/Application/Services/Facades/UserInfo.php
@@ -3,16 +3,39 @@
 namespace Voyanara\LaravelApiClient\Application\Services\Facades;
 
 use Voyanara\LaravelApiClient\Application\Actions\UserInfo\GetUserInfoAction;
+use Voyanara\LaravelApiClient\Application\Actions\UserInfo\GetUserOperationHistoryAction;
+use Voyanara\LaravelApiClient\Domain\Exceptions\ClientResponseException;
+use Voyanara\LaravelApiClient\Domain\Exceptions\TokenValidException;
+use Voyanara\LaravelApiClient\Presentation\Responses\UserInfo\UserInfoOperationsResponse;
 use Voyanara\LaravelApiClient\Presentation\Responses\UserInfo\UserInfoSelfResponse;
 
 readonly class UserInfo
 {
     public function __construct(
-        public GetUserInfoAction $getUserInfoAction
-    ) {}
+        public GetUserInfoAction             $getUserInfoAction,
+        public GetUserOperationHistoryAction $getUserOperationHistoryAction,
+    )
+    {
+    }
 
+    /**
+     * @throws ClientResponseException
+     * @throws TokenValidException
+     */
     public function self(): UserInfoSelfResponse
     {
         return $this->getUserInfoAction->handle();
+    }
+
+    /**
+     * @param string $dateTimeFrom
+     * @param string $dateTimeTo
+     * @return UserInfoOperationsResponse
+     * @throws ClientResponseException
+     * @throws TokenValidException
+     */
+    public function getOperationsHistory(string $dateTimeFrom, string $dateTimeTo): UserInfoOperationsResponse
+    {
+        return $this->getUserOperationHistoryAction->handle($dateTimeFrom, $dateTimeTo);
     }
 }
