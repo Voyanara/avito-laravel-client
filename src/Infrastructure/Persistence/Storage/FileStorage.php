5ded19ac2fbe2091fb5a124192d6b345e007bd25
Voyanara <darkallenb1394@gmail.com>
2024-11-02 04:39:55 +0300

    code optimization

--- src/Infrastructure/Persistence/Storage/FileStorage.php
+++ src/Infrastructure/Persistence/Storage/FileStorage.php
@@ -4,53 +4,49 @@ namespace Voyanara\LaravelApiClient\Infrastructure\Persistence\Storage;
 
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Facades\Storage;
+use JsonException;
 use Override;
 use Voyanara\LaravelApiClient\Domain\Interfaces\TokenStorageInterface;
 use Voyanara\LaravelApiClient\Presentation\Responses\TokenResponse;
 
 readonly class FileStorage implements TokenStorageInterface
 {
+    private string $filePath;
+    public function __construct() {
+        $this->filePath = 'avito_module/'.config('avito.file_name').'.json';
+    }
+
     #[Override]
     public function get(string $clientId, string $clientSecret): ?string
     {
-        $filePath = 'avito_module/'.config('avito.file_name').'.json';
-
         try {
-            $data = Storage::disk('local')->get($filePath);
-
-            if ($data === null) {
-                return null;
-            }
-
-            $tokens = json_decode($data, true);
-
-            if (! is_array($tokens)) {
-                return null;
-            }
-
-            $key = md5($clientId).'::'.md5($clientSecret);
-
+            $tokens = json_decode(Storage::disk('local')->get($this->filePath), true, 512, JSON_THROW_ON_ERROR);
+            $key = $this->generateKey($clientId, $clientSecret);
             return $tokens[$key] ?? null;
-        } catch (FileNotFoundException) {
+        } catch (FileNotFoundException | JsonException) {
             return null;
         }
     }
 
+
+    /**
+     * @throws JsonException
+     */
     #[Override]
     public function set(TokenResponse $tokenResponse, string $clientId, string $clientSecret): void
     {
-        $filePath = 'avito_module/'.config('avito.file_name').'.json';
-
         try {
-            $data = Storage::disk('local')->get($filePath);
-            $tokens = json_decode((string) $data, true);
-        } catch (FileNotFoundException) {
-
+            $tokens = json_decode(Storage::disk('local')->get($this->filePath), true, 512, JSON_THROW_ON_ERROR) ?? [];
+        } catch (FileNotFoundException| JsonException) {
             $tokens = [];
         }
-        $key = md5($clientId).'::'.md5($clientSecret);
+        $key = $this->generateKey($clientId, $clientSecret);
         $tokens[$key] = $tokenResponse->accessToken;
-        $json = json_encode($tokens);
-        Storage::disk('local')->put($filePath, $json);
+        Storage::disk('local')->put($this->filePath, json_encode($tokens, JSON_THROW_ON_ERROR));
+    }
+
+    private function generateKey(string $clientId, string $clientSecret): string
+    {
+        return md5($clientId) . '::' . md5($clientSecret);
     }
 }
